91c0e7a80c1d97223c0575d2ab836ccdde8bfc92
milaek <56858892+milaek@users.noreply.github.com>
2021-04-12 15:53:59 -0700

    Update t9.py
    Set up project framework

---
+++ t9.py
@@ -0,0 +1,41 @@
+"""
+Main
+"""
+
+import helper
+
+
+def parse_content(content):
+    return {}
+
+def make_tree(words):
+    return {}
+
+def predict(tree, numbers):
+    return {}
+
+
+if __name__ == '__main__':
+    content = helper.read_content(filename='ngrams-10k.txt')
+
+    # PART 1: Parsing a string into a dictionary.
+    words = parse_content(content)
+
+    # PART 2: Building a trie from a collection of words.
+    tree = make_tree(words)
+
+    while True:
+        # PART 3: Predict words that could follow
+        numbers = helper.ask_for_numbers()
+        predictions = predict(tree, numbers)
+
+        if not predictions:
+            print('No words were found that match those numbers. :(')
+        else:
+            for prediction, frequency in predictions[:10]:
+                print(prediction, frequency)
+
+        response = input('Want to go again? [y/N] ')
+        again = response and response[0] in ('y', 'Y')
+        if not again:
+            break
